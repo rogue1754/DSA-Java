b57b13c0088253bf0daaf790933c4984868f1d33
Rohan Mehrotra <rohanmeh14@gmail.com>
2026-09-22 18:57:06 +0000

    Code challenge submission for question: 374 Guess Number Higher or Lower

---
+++ 374 Guess Number Higher or Lower/374guess-number-higher-or-lower.java
@@ -0,0 +1,26 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left=0, right=n;
+        int mid=0;
+        while(left<=right)
+        {
+            mid= left+(right-left)/2;
+            if(guess(mid)==0)
+            break;
+            else if(guess(mid)==-1)
+            right=mid-1;
+            else
+            left=mid+1;
+        }
+        return mid;
+    }
+}
